d059c91ef83f899a345c74768d50f1a29a76253c
Ondrej Klejch <ondrej.klejch@gmail.com>
2017-10-24 10:11:44 +0100

    Meta learner can adapt models in multiple steps.

--- learning_to_adapt/utils/data_generator.py
+++ learning_to_adapt/utils/data_generator.py
@@ -3,7 +3,7 @@ import kaldi_io
 import collections
 
 
-def load_data(params, feats, utt2spk, adapt_pdfs, test_pdfs, num_frames=1000):
+def load_data(params, feats, utt2spk, adapt_pdfs, test_pdfs, num_frames=1000, steps=1):
     utt_to_adapt_pdfs = load_utt_to_pdfs(adapt_pdfs)
     utt_to_test_pdfs = load_utt_to_pdfs(test_pdfs)
     utt_to_spk = load_utt_to_spk(utt2spk)
@@ -26,10 +26,10 @@ def load_data(params, feats, utt2spk, adapt_pdfs, test_pdfs, num_frames=1000):
         adapt_pdfs[spk].append(utt_adapt_pdfs)
         test_pdfs[spk].append(utt_test_pdfs)
 
-    return generate_batches(params, feats, adapt_pdfs, test_pdfs, num_frames)
+    return generate_batches(params, feats, adapt_pdfs, test_pdfs, num_frames, steps)
 
 
-def generate_batches(params, feats, adapt_pdfs, test_pdfs, num_frames):
+def generate_batches(params, feats, adapt_pdfs, test_pdfs, num_frames, steps):
     adapt_x = []
     adapt_y = []
     test_x = []
@@ -41,15 +41,14 @@ def generate_batches(params, feats, adapt_pdfs, test_pdfs, num_frames):
         spk_test_pdfs = np.concatenate(test_pdfs[spk])
 
         for offset in range(0, spk_feats.shape[0] - 2 * num_frames, num_frames):
-            adapt_x.append(spk_feats[offset:offset + num_frames])
-            adapt_y.append(spk_adapt_pdfs[offset:offset + num_frames])
+            adapt_x.append([spk_feats[offset:offset + num_frames]] * steps)
+            adapt_y.append([spk_adapt_pdfs[offset:offset + num_frames]] * steps)
             test_x.append(spk_feats[offset + num_frames:offset + 2 * num_frames])
             test_y.append(spk_test_pdfs[offset + num_frames:offset + 2 * num_frames])
 
-    # TODO: allow to adapt for multiple epochs by replacing expand_dims with repeat.
     params = np.array([params] * len(adapt_x))
-    adapt_x = np.expand_dims(np.array(adapt_x), 1)
-    adapt_y = np.expand_dims(np.array(adapt_y), 1)
+    adapt_x = np.array(adapt_x)
+    adapt_y = np.array(adapt_y)
     test_x = np.array(test_x)
     test_y = np.array(test_y)
 
