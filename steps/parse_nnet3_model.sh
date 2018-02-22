73aa75640db18a22562c1e5cc396dca6d0864888
Ondrej Klejch <ondrej.klejch@gmail.com>
2018-02-22 10:48:07 +0000

    Other important files are copied when parsin nnet3 models.

--- steps/parse_nnet3_model.sh
+++ steps/parse_nnet3_model.sh
@@ -14,6 +14,6 @@ if [ -f $src_dir/frame_subsampling_factor ]; then
   frame_subsampling=$(cat $src_dir/frame_subsampling_factor)
 fi
 
-cp -r $src_dir/{final.mdl,graph} $out_dir
+cp -r $src_dir/{final.mdl,graph,cmvn_opts,frame_subsampling_factor} $out_dir
 nnet3-copy --binary=false --prepare-for-test=true $out_dir/final.mdl $out_dir/final.txt
 python steps/parse_nnet3_model.py $out_dir $frame_subsampling $with_lhuc_layers
