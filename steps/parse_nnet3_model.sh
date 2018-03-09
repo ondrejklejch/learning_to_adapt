0f934b1793141d35f6443c994184705385a09dd1
Ondrej Klejch <ondrej.klejch@gmail.com>
2018-03-09 14:28:26 +0000

    Missing files are created when parsing NNET3 models.

--- steps/parse_nnet3_model.sh
+++ steps/parse_nnet3_model.sh
@@ -14,6 +14,7 @@ if [ -f $src_dir/frame_subsampling_factor ]; then
   frame_subsampling=$(cat $src_dir/frame_subsampling_factor)
 fi
 
-cp -r $src_dir/{final.mdl,graph,cmvn_opts,frame_subsampling_factor} $out_dir
+echo $frame_subsampling > $out_dir/frame_subsampling_factor
+cp -r $src_dir/{final.mdl,graph,cmvn_opts,pdf_counts} $out_dir
 nnet3-copy --binary=false --prepare-for-test=true $out_dir/final.mdl $out_dir/final.txt
 python steps/parse_nnet3_model.py $out_dir $frame_subsampling $with_lhuc_layers
