6c4cb6c5bccf85d1f4b57d9e9171adf49b726d02
Ondrej Klejch <ondrej.klejch@gmail.com>
2018-03-19 18:38:22 +0000

    adapt_and_decode.sh works for both NNET1 and NNET3 models.

--- steps/adapt_and_decode.py
+++ steps/adapt_and_decode.py
@@ -1,23 +1,6 @@
-#!/usr/bin/env python
-
-##  Copyright (C) 2016 D S Pavan Kumar
-##  dspavankumar [at] gmail [dot] com
-##
-##  This program is free software: you can redistribute it and/or modify
-##  it under the terms of the GNU General Public License as published by
-##  the Free Software Foundation, either version 3 of the License, or
-##  (at your option) any later version.
-##
-##  This program is distributed in the hope that it will be useful,
-##  but WITHOUT ANY WARRANTY; without even the implied warranty of
-##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##  GNU General Public License for more details.
-##
-##  You should have received a copy of the GNU General Public License
-##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import itertools
 import json
+import math
 from signal import signal, SIGPIPE, SIG_DFL
 import sys
 
@@ -26,14 +9,13 @@ import keras
 import kaldi_io
 import tensorflow as tf
 
-from learning_to_adapt.model import FeatureTransform, LHUC, load_meta_learner, get_model_weights, set_model_weights
-from learning_to_adapt.utils import load_utt_to_pdfs
+from learning_to_adapt.model import FeatureTransform, LHUC, Renorm, load_meta_learner, get_model_weights, set_model_weights
+from learning_to_adapt.utils import load_utt_to_pdfs, pad_feats, create_chunks
 
 config = tf.ConfigProto()
 config.intra_op_parallelism_threads=1
 config.inter_op_parallelism_threads=1
 keras.backend.tensorflow_backend.set_session(tf.Session(config=config))
-
 
 def adapt(model, method, config, x, y):
     config = load_config(config)
@@ -49,22 +31,22 @@ def adapt(model, method, config, x, y):
 
 
 def adapt_all(model, config, x, y):
-    model.compile(loss='sparse_categorical_crossentropy', optimizer=keras.optimizers.SGD(lr=config["lr"]))
-    model.fit(x, y, batch_size=256, epochs=config["epochs"], verbose=0)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=keras.optimizers.SGD(lr=2.5e-5))
+    model.fit(x, y, batch_size=256, epochs=3, verbose=0)
 
 
 def adapt_lhuc(model, config, x, y):
     for l in model.layers:
         l.trainable = l.name.startswith('lhuc')
 
-    model.compile(loss='sparse_categorical_crossentropy', optimizer=keras.optimizers.SGD(lr=config["lr"]))
-    model.fit(x, y, batch_size=256, epochs=config["epochs"], verbose=0)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=keras.optimizers.SGD(lr=0.7))
+    model.fit(x, y, batch_size=256, epochs=3, verbose=0)
 
 
 def adapt_meta(model, config, x, y):
     params = get_model_weights(model).reshape((1, -1))
-    x = x.reshape((1, 1, -1, x.shape[-1]))
-    y = y.reshape((1, 1, -1, y.shape[-1]))
+    x = x.reshape((1, 1) + x.shape)
+    y = y.reshape((1, 1) + y.shape)
 
     epochs = config.get("epochs", 1)
     x = np.repeat(x, epochs, axis=1)
@@ -85,40 +67,58 @@ if __name__ == '__main__':
     adaptation_pdfs = sys.argv[3]
     adaptation_frames = int(sys.argv[4])
     model = sys.argv[5]
-    priors = sys.argv[6]
+    counts = sys.argv[6]
+    frame_subsampling_factor = int(sys.argv[7])
+    left_context = int(sys.argv[8])
+    right_context = int(sys.argv[9])
+    chunk_size = 50
 
     if not model.endswith('.h5'):
         raise TypeError ('Unsupported model type. Please use h5 format. Update Keras if needed')
 
     ## Load model
-    m = keras.models.load_model(model, custom_objects={'FeatureTransform': FeatureTransform, 'LHUC': LHUC})
-    p = np.genfromtxt(priors, delimiter=',')
+    m = keras.models.load_model(model, custom_objects={'FeatureTransform': FeatureTransform, 'LHUC': LHUC, 'Renorm': Renorm})
+
+    with open(counts, 'r') as f:
+        counts = np.fromstring(f.read().strip(" []"), dtype='float32', sep=' ')
+	priors = counts / np.sum(counts)
 
     with kaldi_io.SequentialBaseFloatMatrixReader("ark:-") as arkIn, \
             kaldi_io.BaseFloatMatrixWriter("ark,t:-") as arkOut:
         signal(SIGPIPE, SIG_DFL)
 
         # Reads adaptation_frames from first utterances to obtain adaptation data.
+        num_chunks = adaptation_frames / chunk_size
         utt_to_pdfs = load_utt_to_pdfs(adaptation_pdfs)
-        feats = []
-        pdfs = []
-        utt_buffer = []
-        for utt, utt_feats in arkIn:
-            feats.append(utt_feats)
-            pdfs.append(utt_to_pdfs[utt])
-            utt_buffer.append((utt, utt_feats))
 
-            if np.concatenate(feats).shape[0] >= adaptation_frames:
+        chunks = []
+        utt_buffer = []
+        for utt, feats in arkIn:
+            utt_buffer.append((utt, feats))
+
+            pdfs = utt_to_pdfs[utt]
+            chunks.extend(create_chunks(feats, pdfs, pdfs, chunk_size, left_context, right_context, frame_subsampling_factor))
+
+            if len(chunks) >= num_chunks:
                 break
 
         # Adapts model using adaptation data
-        feats = np.concatenate(feats)[:adaptation_frames]
-        pdfs = np.concatenate(pdfs)[:adaptation_frames]
+        feats = np.stack([x[0] for x in chunks[:num_chunks]])
+        pdfs = np.stack([x[1] for x in chunks[:num_chunks]])
+
+        before = np.mean(np.argmax(m.predict(feats), axis=-1).flatten() == pdfs.flatten())
+        print >> sys.stderr, "BEFORE", before
         adapt(m, adaptation_method, adaptation_config, feats, pdfs)
+
+        after = np.mean(np.argmax(m.predict(feats), axis=-1).flatten() == pdfs.flatten())
+        print >> sys.stderr, "AFTER", after
+        print >> sys.stderr, "IMPROVEMENT", after - before
 
         # Decodes everything with the adapted model.
         arkIn = itertools.chain(utt_buffer, arkIn)
-        for utt, utt_feats in arkIn:
-            logProbMat = np.log(m.predict(utt_feats) / p)
+        for utt, feats in arkIn:
+            feats = np.expand_dims(pad_feats(feats, left_context, right_context), 0)
+
+            logProbMat = np.log(m.predict(feats)[0] / priors)
             logProbMat[logProbMat == -np.inf] = -100
             arkOut.write(utt, logProbMat)
--- steps/adapt_and_decode.sh
+++ steps/adapt_and_decode.sh
@@ -18,7 +18,7 @@ model_dir=$6
 graph_dir=$7
 decode_dir=$8
 
-for f in $graph_dir/HCLG.fst $data/feats.scp $model_dir/tree; do
+for f in $graph_dir/HCLG.fst $data/feats.scp; do
   [ ! -f $f ] && echo "$0: no such file $f" && exit 1;
 done
 
@@ -26,15 +26,22 @@ done
 mkdir -p $decode_dir
 ali-to-pdf $model_dir/final.mdl ark:"gunzip -c $adaptation_ali/ali.*.gz |" ark,t:$decode_dir/pdfs
 
+cmvn_opts=`cat $model_dir/cmvn_opts`
+delta_opts=`cat $model_dir/delta_opts`
+splice_opts=`cat $model_dir/splice_opts`
+context_opts=`cat $model_dir/context_opts`
+frame_subsampling_factor=`cat $model_dir/frame_subsampling_factor`
+
 sdata=$data/spks_split
 adaptation_pdfs="ark:$decode_dir/pdfs"
-feats="ark,s,cs:apply-cmvn --norm-vars=true --utt2spk=ark:$sdata/JOB/utt2spk scp:$sdata/JOB/cmvn.scp scp:$sdata/JOB/feats.scp ark:- |"
-$add_deltas && feats="$feats add-deltas ark:- ark:- |"
-$splice_feats && feats="$feats splice-feats --left-context=5 --right-context=5 ark:- ark:- |"
-feats="$feats python steps/adapt_and_decode.py $method $config $adaptation_pdfs $adaptation_frames $model_dir/dnn.nnet.h5 $model_dir/dnn.priors.csv |"
+feats="ark,s,cs:apply-cmvn $cmvn_opts --utt2spk=ark:$sdata/JOB/utt2spk scp:$sdata/JOB/cmvn.scp scp:$sdata/JOB/feats.scp ark:- |"
+feats="$feats add-deltas $delta_opts ark:- ark:- |"
+feats="$feats splice-feats $splice_opts ark:- ark:- |"
+feats="$feats python steps/adapt_and_decode.py $method $config $adaptation_pdfs $adaptation_frames $model_dir/dnn.nnet.h5 $model_dir/pdf_counts $frame_subsampling_factor $context_opts |"
 feats="$feats grep -v 'import dot_parser' |"
+decode_opts="--max-active=$max_active --beam=$beam --lattice-beam=$latbeam --acoustic-scale=$acwt --allow-partial=true"
+lat_wspecifier="ark:| gzip -c > $decode_dir/lat.JOB.gz"
 
 num_spks=`cat $data/spks_list | wc -l`
 $cmd JOB=1:$num_spks $decode_dir/log/decode.JOB.log \
-  latgen-faster-mapped --max-active=$max_active --beam=$beam --lattice-beam=$latbeam --acoustic-scale=$acwt --allow-partial=true --word-symbol-table=$graph_dir/words.txt $model_dir/final.mdl $graph_dir/HCLG.fst "$feats" "ark:|gzip -c > $decode_dir/lat.JOB.gz"
-
+  latgen-faster-mapped $decode_opts --word-symbol-table=$graph_dir/words.txt $model_dir/final.mdl $graph_dir/HCLG.fst "$feats" "$lat_wspecifier"
