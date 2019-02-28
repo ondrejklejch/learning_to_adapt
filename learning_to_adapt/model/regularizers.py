e295bb8eb6e93069e8c93dfb542984adb0455cb7
Ondrej Klejch <ondrej.klejch@gmail.com>
2019-02-28 11:49:46 +0000

    L0 regularizer fixed.

--- learning_to_adapt/model/regularizers.py
+++ learning_to_adapt/model/regularizers.py
@@ -11,12 +11,7 @@ class L0(Regularizer):
         self.delta = delta
 
     def __call__(self, loga):
-        l0 = self.l0 * K.sum(K.sigmoid(loga - self.beta * K.log(-self.gamma / self.delta)))
-
-        if K.ndim(loga) > 1:
-            return l0 / K.cast(K.shape(loga)[0], K.floatx())
-        else:
-            return l0
+        return self.l0 * K.sum(K.sigmoid(loga - self.beta * K.log(-self.gamma / self.delta)))
 
     def get_config(self):
         return {
