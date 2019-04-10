e55aaffcb7a3740d51890b70e0dc9987842f6d49
Ondrej Klejch <ondrej.klejch@gmail.com>
2019-04-10 15:32:48 +0100

    Model loader sets trainable layers according to adaptation type.

--- learning_to_adapt/model/__init__.py
+++ learning_to_adapt/model/__init__.py
@@ -6,8 +6,8 @@ from maml import *
 from regularizers import *
 from wrapper import *
 
-def load_model(path):
-    return keras_load_model(path, custom_objects={
+def load_model(path, adaptation_type=None):
+    model = keras_load_model(path, custom_objects={
         'FeatureTransform': FeatureTransform,
         'LHUC': LHUC,
         'SparseLHUC': SparseLHUC,
@@ -16,7 +16,22 @@ def load_model(path):
         'SparseMultiply': SparseMultiply,
         'SDBatchNormalization': SDBatchNormalization,
         'UttBatchNormalization': UttBatchNormalization,
+        'LDA': LDA,
         'L0': L0,
+        'MetaLearner': MetaLearner,
+        'LearningRatePerLayerMetaLearner': LearningRatePerLayerMetaLearner,
         'MAML': MAML,
         'ModelWrapper': ModelWrapper,
     })
+
+    if adaptation_type == "LHUC":
+        for l in model.layers:
+            l.trainable = l.name.startswith("lhuc")
+    elif adaptation_type == "BATCHNORM":
+        for l in model.layers:
+            l.trainable = l.name.endswith("batchnorm")
+    elif adaptation_type == "ALL":
+        for l in model.layers:
+            l.trainable = l.name != "lda" and not l.name.startswith("lhuc")
+
+    return model
